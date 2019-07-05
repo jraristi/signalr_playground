2143685dcf05a890072778b7760bc9212071e32e
John Aristizabal <j.aristizabal@dasgip.de>
2019-07-05 16:09:41 +0200

    first commit: project added
    Issue: VNCL-1727

---
+++ Eppendorf.VNCloud.StatusDataPushService/Program.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Program.cs" company="Eppendorf AG - 2018">
+// Copyright (c) Eppendorf AG - 2018. All rights reserved.
+// </copyright>
+
+namespace Eppendorf.VNCloud.StatusDataPushService
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Eppendorf.VNCloud.StatusDataPushService/Startup.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="Startup.cs" company="Eppendorf AG - 2018">
+// Copyright (c) Eppendorf AG - 2018. All rights reserved.
+// </copyright>
+
+namespace Eppendorf.VNCloud.StatusDataPushService
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddApiVersioning(o =>
+                {
+                    o.ReportApiVersions = true;
+                    o.AssumeDefaultVersionWhenUnspecified = true;
+                    o.DefaultApiVersion = new ApiVersion(1, 0);
+                });
+            services.AddHealthChecks();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHealthChecks("/api/heartbeat", new HealthCheckOptions()
+            {
+                AllowCachingResponses = false,
+                ResponseWriter = WriteResponse
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        private static Task WriteResponse(
+            HttpContext httpContext,
+            HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    {
+                        return new JProperty(pair.Key, new JObject(
+                            new JProperty("status", pair.Value.Status.ToString()),
+                            new JProperty("description", pair.Value.Description),
+                            new JProperty("data", new JObject(pair.Value.Data.Select(
+                                p => new JProperty(p.Key, p.Value))))));
+                    }))));
+            return httpContext.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
+        }
+    }
+}
